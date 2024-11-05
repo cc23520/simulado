a1033fc83fa9e8c4f7159ce45648254316099e89
cc23520 <cc23520@g.unicamp.br>
2024-11-05 19:17:16 -0300

    a

---
+++ demo/src/test/java/com/simulado/demo/SimuladoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.simulado.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimuladoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
