db8a03ce632fee93231de0917d965670a01e45cf
cc23520 <cc23520@g.unicamp.br>
2024-11-05 20:21:18 -0300

    put post

--- demo/src/main/java/com/simulado/demo/Repository/ProductCategoryRepository.java
+++ demo/src/main/java/com/simulado/demo/Repository/ProductCategoryRepository.java
@@ -4,7 +4,10 @@ import com.simulado.demo.Model.Product;
 import com.simulado.demo.Model.ProductCategory;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface ProductCategoryRepository extends JpaRepository<ProductCategory, Integer> {
 
 
+    void delete(Optional<ProductCategory> pd);
 }
